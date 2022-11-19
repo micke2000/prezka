14a654bd509ab33af1be11bc67210e00e1827138
Michał Mickiewicz <65499633+micke2000@users.noreply.github.com>
2022-11-19 12:55:58 +0100

    Update srp.ts

--- clean-code-rules/srp.ts
+++ clean-code-rules/srp.ts
@@ -1,32 +1,32 @@
-export const assignToNeighboursQuality = (
+export const assignmentQuality = (
   group: IGroup,
-  rooms: IRoom[],
+  boxes: IBox[],
   { ...inputProps }: IQualityPropsForQuality
 ) => {
   const inputPropsTemp = { ...inputProps };
-  let availableRooms = rooms.filter((room: IRoom) => !inputPropsTemp.assignedRoomsNumbers.includes(room.number));
-  availableRooms = availableRooms.filter((room: IRoom) => room.beds < group.members.length);
+  let availableRooms = boxes.filter((box: IBox) => !inputPropsTemp.assignedBoxesNumber.includes(box.number));
+  availableRooms = availableRooms.filter((box: IBox) => box.capacity < group.package.length);
   let assignment: IAssignmentDictionary | null = null;
   const assignedNumbers: string[] = [];
   const foundSmallerWithNeighbours = [...Array.from(Array(availableRooms.length + 1).keys()).slice(2)].some((i) => {
-    let currentDepthGroups: IRoomsGroupWithNeighbours[] = [];
-    availableRooms.forEach((rm: IRoom) => {
-      const configuartions = getAllRoomWithNeigboursConfigurations(rm, i, availableRooms);
-      configuartions.forEach((conf: IRoom[]) => {
-        const bedsInGroup = conf.reduce((acc, room: IRoom) => acc + room.beds, 0);
-        if (conf.length === i && bedsInGroup >= group.members.length) {
-          currentDepthGroups.push({ rooms: conf, bedsAmmount: bedsInGroup });
+    let currentDepthGroups: IBoxsGroupWithNeighbours[] = [];
+    availableRooms.forEach((rm: IBox) => {
+      const configuartions = getAllBoxesWithNeighboursConfiguration(rm, i, availableRooms);
+      configuartions.forEach((conf: IBox[]) => {
+        const capacityInGroup = conf.reduce((acc, box: IBox) => acc + box.capacity, 0);
+        if (conf.length === i && capacityInGroup >= group.package.length) {
+          currentDepthGroups.push({ boxes: conf, capacityAmmount: capacityInGroup });
         }
       });
     });
     if (currentDepthGroups.length > 0) {
-      currentDepthGroups = eliminateDuplicatesOfRoomGroups(sortArrayOfRoomGroupsByNumbers(currentDepthGroups));
-      currentDepthGroups = sortArrayOfRoomGroupsByBedsAmmount(currentDepthGroups);
+      currentDepthGroups = eliminateDuplicatesOfB(sortArrayOfBoxesByNumbers(currentDepthGroups));
+      currentDepthGroups = sortArrayOfBoxesByCapacity(currentDepthGroups);
       assignment = {
         group,
-        rooms: currentDepthGroups[0].rooms,
+        boxes: currentDepthGroups[0].boxes,
       } as IAssignmentDictionary;
-      assignedNumbers.push(...currentDepthGroups[0].rooms.map((rm) => rm.number));
+      assignedNumbers.push(...currentDepthGroups[0].boxes.map((rm) => rm.number));
       return true;
     }
     return false;
@@ -40,7 +40,7 @@ export const assignToNeighboursQuality = (
       assignemnt: assignment,
     };
     inputPropsTemp.stageAssignments.push(...assignedNumbers);
-    inputPropsTemp.assignedRoomsNumbers = inputPropsTemp.assignedRoomsNumbers.concat(assignedNumbers);
+    inputPropsTemp.assignedBoxesNumber = inputPropsTemp.assignedBoxesNumber.concat(assignedNumbers);
     inputPropsTemp.assigned = true;
   }
   return inputPropsTemp;
